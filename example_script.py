17c22a08764fe547506952e95ac123b19ee31223
Andrea Valentini <nvlent@amazon.com>
2021-01-06 17:53:59 +0100

    rero

---
+++ example_script.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 27 12:11:03 2020
+
+@author: nvlent
+"""
+
+import sys
+
+
+sys.path.append(r'C:\Andrea')
+
+#sys.path
+
+import example_package
+
+
+example_package.example_sub_pack_1.module_1.a
+
+
+
+new branch mod
+
+
+second branch mod
+
+
+
+new_keep_branch
+
